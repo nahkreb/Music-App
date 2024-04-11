97846ca84a1cbe14f99c525af2adb7a5a2ab5842
Berkhan <123936730+nahkreb@users.noreply.github.com>
2024-04-12 00:55:46 +0300

    Music App
    Music application created with MAUI technology developed by ASP.NET.

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 using Plugin.Maui.Audio;
 
+
 namespace MusicApp.Pages;
 
 public partial class MainPage : ContentPage
@@ -15,20 +16,44 @@ public partial class MainPage : ContentPage
 
     }
 
+
+
+    private bool isPlaying = false; // Müziðin çalýp çalmadýðýný izlemek için bir deðiþken
+
     private async void PlayAndPauseButton_Clicked(object sender, EventArgs e)
     {
+
+
         if (SelectedSongLabel.Text == "Sezen Aksu - Ben de Yoluma Giderim")
         {
             var player = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("audio.m4a"));
 
-            player.Play();
+            if (isPlaying)
+            {
+                // Müzik çalýyorsa, oynatýcýyý durdur
+                player.Pause();
+                isPlaying = false;
+
+            }
+            else
+            {
+                // Müzik çalmýyorsa, oynatýcýyý baþlat
+                player.Play();
+                isPlaying = true;
+
+                var defaultMusicDuration = TimeSpan.FromSeconds(player.Duration); // Video süresini saniye cinsinden al
+                Lbl_MusicDuration.Text = defaultMusicDuration.ToString(@"mm\:ss"); // Dakika: Saniye formatýnda göster
+
+
+
+            }
         }
         else
         {
-            DisplayAlert("Hata", "Bu Þarký Malesef Bulunamamaktadýr.Lütfen Baþka Bir Þarký Seçin", "Tamam");
-
+            await DisplayAlert("Hata", "Bu Þarký Malesef Bulunamamaktadýr. Lütfen Baþka Bir Þarký Seçin", "Tamam");
         }
     }
+
 
     private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
